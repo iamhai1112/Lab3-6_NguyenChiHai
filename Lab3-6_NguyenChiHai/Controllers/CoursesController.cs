56c9b7e0ad3a32c0237b778ece4d1a016f51ff67
Admin <Admin@DESKTOP-N2SIEMN>
2023-05-26 10:59:12 +0700

    26/5

--- Lab3-6_NguyenChiHai/Controllers/CoursesController.cs
+++ Lab3-6_NguyenChiHai/Controllers/CoursesController.cs
@@ -1,5 +1,6 @@
 ﻿using Lab3_6_NguyenChiHai.Models;
 using Lab3_6_NguyenChiHai.ViewModels;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +17,7 @@ namespace Lab3_6_NguyenChiHai.Controllers
             _dbContext = new ApplicationDbContext();
         }
         // GET: Courses
+        [Authorize]
         public ActionResult Create()
         {
             var viewModel = new CourseViewModel
@@ -23,6 +25,28 @@ namespace Lab3_6_NguyenChiHai.Controllers
                 Categories = _dbContext.Categories.ToList()
             };
             return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create (CourseViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Categories = _dbContext.Categories.ToList();
+                return View("Create", viewModel);
+            }
+            var course = new Course
+            {
+                LecturerId = User.Identity.GetUserId(),
+                DateTime = viewModel.GetDateTime(),
+                CategoryId = viewModel.Category,
+                Place = viewModel.Place
+            };
+            _dbContext.Courses.Add(course);
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- Lab3-6_NguyenChiHai/ViewModels/CourseViewModel.cs
+++ Lab3-6_NguyenChiHai/ViewModels/CourseViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Lab3_6_NguyenChiHai.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,16 @@ namespace Lab3_6_NguyenChiHai.ViewModels
 {
     public class CourseViewModel
     {
+        //26/5
+        //heheh
+        [Required]
         public string Place { get; set; }
+        [Required]
+        [FutureDate]
         public string Date { get; set; }
+        [Required]
         public string Time { get; set; }
+        [Required]
         public byte Category { get; set; }
         public IEnumerable<Category> Categories { get; set; }
         public DateTime GetDateTime()
